834388e01c98d739ff77f9fdad2885fce4510f1e
Faozan Fahmi Ardhana <146506099+Faozanfa12@users.noreply.github.com>
2024-12-27 19:32:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika hambuger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hambuger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hambuger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
